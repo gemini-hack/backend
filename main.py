e86258e1c23dd8290bc8952e3033f341ac8d64e2
Lexmanthefirst <lexmanthefirst@gmail.com>
2026-01-25 21:50:32 +0100

    Feat: Configure CORS to allow all origins

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
